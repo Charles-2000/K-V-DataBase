e949de1a750c4e8ca3644b2a53f2792a684ab88a
Charles-2000 <57382494+Charles-2000@users.noreply.github.com>
2019-12-04 14:56:41 +0800

    Update KVDB.cpp

--- Stage 2/KVDB.cpp
+++ Stage 2/KVDB.cpp
@@ -10,8 +10,11 @@ using namespace std;
 KVDBHandler::KVDBHandler(const std::string& db_file)
 {
 	file.open(db_file.c_str(), ios::out | ios::binary | ios::app); //If file doesn't exist, create file.
-	if (file.fail())
+	if (file.fail())  //if fail, throws exception and exits program
+	{
+		throw "Create file \"" + db_file + "There's no space left on devices!";
 		exit(KVDB_NO_SPACE_LEFT_ON_DEVICES);
+	}	
 	else
 	{
 		file_path = db_file;
@@ -25,7 +28,7 @@ KVDBHandler::~KVDBHandler()
 {
 	if (file)
 		file.close();
-	AOF_index.clear();
+	this->AOF_index.clear();
 }
 
 string KVDBHandler::getFilePath()
@@ -33,39 +36,51 @@ string KVDBHandler::getFilePath()
 	return file_path;
 }
 
-fstream& KVDBHandler::get_db_file()
+fstream* KVDBHandler::get_db_file()
 {
-	return this->file;
+	return &this->file;
+}
+
+void KVDBHandler::openFile()
+{
+	if(!file)
+		file.open(file_path.c_str(), ios::binary | ios::in | ios::out | ios::app);
+}
+
+void KVDBHandler::closeFile()
+{
+	if (file)
+		file.close();
 }
 
 int KVDBHandler::createAOFIndex()
 {
-	fstream& f = this->get_db_file();
-	f.seekg(0, ios::beg);
+	fstream* f = this->get_db_file();
+	f->seekg(0, ios::beg);
 
 	uint32_t keyl, valuel;
 	string _key;
 
-	while (f.peek() != EOF)
+	while (f->peek() != EOF)
 	{
-		int pos = f.tellg();  //records the offset of a new K-V pair
+		int pos = f->tellg();  //records the offset of a new K-V pair
 
-		f.read(reinterpret_cast<char*>(&keyl), sizeof(uint32_t));
-		f.read(reinterpret_cast<char*>(&valuel), sizeof(uint32_t));
+		f->read(reinterpret_cast<char*>(&keyl), sizeof(uint32_t));
+		f->read(reinterpret_cast<char*>(&valuel), sizeof(uint32_t));
 
 		char* tmp = new char[keyl + 1];
-		f.read(tmp, keyl * sizeof(char));
+		f->read(tmp, keyl * sizeof(char));
 		tmp[keyl] = '\0';
 		_key = tmp;
 		delete[]tmp;
 
 		//if value exists, update position; else delete key
 		if (valuel != 0)
-			AOF_index[_key] = pos;
+			this->AOF_index[_key] = pos;
 		else
-			AOF_index.erase(_key);
+			this->AOF_index.erase(_key);
 
-		f.seekg(valuel, ios::cur);  //skips value
+		f->seekg(valuel, ios::cur);  //skips value
 	}
 
 	/*//for test
@@ -76,10 +91,10 @@ int KVDBHandler::createAOFIndex()
 	return KVDB_OK;
 }
 
-/*unordered_map<string, int> KVDBHandler::getAOFIndex()
+unordered_map<string, int>* KVDBHandler::getAOFIndex()
 {
-	return this->AOF_index;
-}*/
+	return &this->AOF_index;
+}
 
 void KVDBHandler::setOffset(const std::string key, const int offset)
 {
@@ -88,9 +103,9 @@ void KVDBHandler::setOffset(const std::string key, const int offset)
 
 int KVDBHandler::getOffset(const std::string key)
 {
-	unordered_map<string, int>::iterator it = AOF_index.find(key);
-	if (it != AOF_index.end())
-		return AOF_index[key];
+	unordered_map<string, int>::iterator it = this->AOF_index.find(key);
+	if (it != this->AOF_index.end())
+		return it->second;
 	return -1;
 }
 
@@ -104,9 +119,9 @@ int kvdb::set(KVDBHandler* handler, const std::string& key, const std::string& v
 	if (key.length() == 0 || key.length() > MAX_SIZE)
 		return KVDB_INVALID_KEY;
 	
-	fstream &f = handler->get_db_file();
-	f.seekg(0, ios::end);	//locate the pointer to the end of file
-	int pos = f.tellg();  //records current position
+	fstream* f = handler->get_db_file();
+	f->seekg(0, ios::end);	//locate the pointer to the end of file
+	int pos = f->tellg();  //records current position
 	
 	//set data messages
 	uint32_t key_length = key.length();
@@ -114,10 +129,10 @@ int kvdb::set(KVDBHandler* handler, const std::string& key, const std::string& v
 	string _key = key;
 	string _value = value;
 
-	f.write(reinterpret_cast<char*>(&key_length), sizeof(uint32_t));
-	f.write(reinterpret_cast<char*>(&value_length), sizeof(uint32_t));
-	f.write(_key.c_str(), key_length * sizeof(char));
-	f.write(_value.c_str(), value_length * sizeof(char));
+	f->write(reinterpret_cast<char*>(&key_length), sizeof(uint32_t));
+	f->write(reinterpret_cast<char*>(&value_length), sizeof(uint32_t));
+	f->write(_key.c_str(), key_length * sizeof(char));
+	f->write(_value.c_str(), value_length * sizeof(char));
 
 	handler->setOffset(_key, pos);
 
@@ -133,24 +148,19 @@ int kvdb::get(KVDBHandler* handler, const std::string& key, std::string& value)
 	if(pos == -1)  //key doesn't exist
 		return KVDB_INVALID_KEY;
 
-	fstream& f = handler->get_db_file();
-	f.seekg(pos, ios::beg);	//locate the pointer to the begin of file
+	fstream* f = handler->get_db_file();
+	f->seekg(pos, ios::beg);	//locate the pointer to the begin of file
 
 	uint32_t keyl, valuel;
-	string _key;
 	
-	f.read(reinterpret_cast<char*>(&keyl), sizeof(uint32_t));
-	f.read(reinterpret_cast<char*>(&valuel), sizeof(uint32_t));
+	f->read(reinterpret_cast<char*>(&keyl), sizeof(uint32_t));
+	f->read(reinterpret_cast<char*>(&valuel), sizeof(uint32_t));
 
-	char* tmp = new char[keyl+1];
-	f.read(tmp, keyl * sizeof(char));
-	tmp[keyl] = '\0';
-	_key = tmp;
-	delete[]tmp;
+	f->seekg(keyl, ios::cur);
 
 	//set value
 	value.resize(valuel);
-	f.read(&value[0], valuel * sizeof(char));
+	f->read(&value[0], valuel * sizeof(char));
 
 	return KVDB_OK;
 }
@@ -167,15 +177,14 @@ int kvdb::del(KVDBHandler* handler, const std::string& key)
 	//If key exists, set data messages.
 	uint32_t key_length = key.length();
 	uint32_t value_length = 0;
-	string _key = key;
 
-	fstream& f = handler->get_db_file();
-	f.seekg(0, ios::end);
-	f.write(reinterpret_cast<char*>(&key_length), sizeof(uint32_t));
-	f.write(reinterpret_cast<char*>(&value_length), sizeof(uint32_t));
-	f.write(_key.c_str(), key_length * sizeof(char));
+	fstream* f = handler->get_db_file();
+	f->seekg(0, ios::end);
+	f->write(reinterpret_cast<char*>(&key_length), sizeof(uint32_t));
+	f->write(reinterpret_cast<char*>(&value_length), sizeof(uint32_t));
+	f->write(key.c_str(), key_length * sizeof(char));
 
-	handler->deleteOffset(_key);
+	handler->deleteOffset(key);
 
 	return KVDB_OK;
 }
@@ -183,31 +192,33 @@ int kvdb::del(KVDBHandler* handler, const std::string& key)
 int kvdb::purge(KVDBHandler* handler)
 {
 	//create temporary file
-	string tmp_path = "tmp.txt";
-	fstream tmp_file(tmp_path.c_str(), ios::out | ios::binary);
-	if (tmp_file.fail())
-		return KVDB_NO_SPACE_LEFT_ON_DEVICES;
+	string tmp_path = "tmp";
 
-	//handler->createAOFIndex();
-	//unordered_map<string, int> Index = handler->getAOFIndex();
-	fstream& f = handler->get_db_file();
+	//determine whether file "tmp" exists
+	fstream tmp_file(tmp_path.c_str(), ios::in);
+	if (!tmp_file.fail())  //if exists, remove file "tmp"
+		remove(tmp_path.c_str());
+
+	fstream* f = handler->get_db_file();
 	kvdb::KVDBHandler tmp_kv(tmp_path);
 
+	unordered_map<string, int>* Index = handler->getAOFIndex();
 	unordered_map<string, int>::iterator it;
-	for (it = handler->AOF_index.begin(); it != handler->AOF_index.end(); it++)
+	for (it = Index->begin(); it != Index->end(); it++)
 	{
 		string key = it->first;
 		string value;
-		if (get(handler, key, value) == KVDB_OK) 
-		{
-			set(&tmp_kv, key, value);
-		}
+
+		get(handler, key, value);
+		set(&tmp_kv, key, value);
 	}
 
 	//update the new Append-Only file and delete the old one
 	string oldpath = handler->getFilePath();
-	rename(oldpath.c_str(), tmp_path.c_str());
+	handler->closeFile();
+	tmp_kv.closeFile();
 	remove(oldpath.c_str());
-
+	rename(tmp_path.c_str(), oldpath.c_str());
+	
 	return KVDB_OK;
 }
