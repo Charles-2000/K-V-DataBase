c1f22b80088b6a2d9e597f2b85264fff5c2e3c72
Charles-2000 <57382494+Charles-2000@users.noreply.github.com>
2019-12-23 10:45:55 +0800

    Update main.cpp

--- Stage 2/main.cpp
+++ Stage 2/main.cpp
@@ -7,23 +7,49 @@ int main(int, char**) {
 	std::string value;
 	kvdb::KVDBHandler kv("db_path");
 
+	//test 1
 	kvdb::set(&kv, "asdf", "123");
 	kvdb::expires(&kv, "asdf", 5);
 	time_t t = time(NULL);
 	std::cout << "Cur:  " << t << std::endl;
 	if (kvdb::KVDB_OK == kvdb::get(&kv, "asdf", value)) {
 		std::cout << value << std::endl;
-	}
-	else
+	}else
 		std::cout << "Not Found!" << std::endl;
 
 	Sleep(6000);
-	t = time(NULL);
-	std::cout << "Cur:  " << t << std::endl;
+
 	if (kvdb::KVDB_OK == kvdb::get(&kv, "asdf", value)) {
 		std::cout << value << std::endl;
-	}
-	else
+	}else
+		std::cout << "Not Found!" << std::endl;
+	std::cout << "------------------------" << std::endl;
+
+
+	//test 2
+	//set expired time for a nonexistent key
+	kvdb::expires(&kv, "asd", 10);
+	if (kvdb::KVDB_OK == kvdb::get(&kv, "asd", value)) {
+		std::cout << value << std::endl;
+	}else
+		std::cout << "Not Found!" << std::endl;
+	std::cout << "------------------------" << std::endl;
+
+
+	//test 3
+	//set expired time(5s) for a key 
+	//reset the key
+	//get key at 6s
+	kvdb::set(&kv, "asdf", "123");
+	kvdb::expires(&kv, "asdf", 5);
+
+	Sleep(3000);
+	kvdb::set(&kv, "asdf", "456");
+	Sleep(3000);
+
+	if (kvdb::KVDB_OK == kvdb::get(&kv, "asdf", value)) {
+		std::cout << value << std::endl;
+	}else
 		std::cout << "Not Found!" << std::endl;
 
 
